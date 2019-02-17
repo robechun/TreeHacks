934b8bf05af51ec1a95ff78ce3117bd464fe7dbe
Jenna <jenna.kim0502@gmail.com>
2019-02-16 20:27:49 -0800

    local change

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -10,6 +10,8 @@ import {
 } from 'blockstack';
 
 import Table from "../Table.jsx";
+import { UserGroup } from 'radiks';
+
 
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 const statusFileName = 'statuses.json'
@@ -65,6 +67,22 @@ export default class Profile extends Component {
     })
   }
 
+  handleUploadNewFile(event) {
+    // TODO: allow user to put in a text file
+    saveNewStatus(text)
+    const group = new UserGroup({ name: 'records-health-' + blockstack.loadUserData().profile.name + this.state.statusIndex}); 
+    console.log(group);
+
+    (async () => {
+        await group.create();
+        console.log('two');
+        const invitation = await group.makeGroupMembership(this.state.usernameToInvite);
+        console.log(invitation._id); // the ID used to later activate an invitation
+        // TODO: give this to the user you want to share it with
+    })();
+  }
+
+
   handleSignOut(e) {
     e.preventDefault();
     signUserOut(window.location.origin);
@@ -82,7 +100,7 @@ export default class Profile extends Component {
 
     statuses.unshift(status)
     const options = { encrypt: false }
-    putFile(statusFileName, JSON.stringify(statuses), options)
+    putFile('statuses.json', JSON.stringify(statuses), options)
       .then(() => {
         this.setState({
           statuses: statuses
@@ -92,10 +110,10 @@ export default class Profile extends Component {
 
   // TODO refactor or delete
   fetchData() {
-    if (this.isLocal()) {
+    // if (this.isLocal()) {
       this.setState({ isLoading: true })
-      const options = { decrypt: false, zoneFileLookupURL: 'https://core.blockstack.org/v1/names/' }
-      getFile(statusFileName, options)
+      const options = { decrypt: false}, zoneFileLookupURL: 'https://core.blockstack.org/v1/names/' }
+      getFile('statuses.json', options)
         .then((file) => {
           var statuses = JSON.parse(file || '[]')
           this.setState({
@@ -108,7 +126,8 @@ export default class Profile extends Component {
         .finally(() => {
           this.setState({ isLoading: false })
         })
-    } else {
+    // } else {
+      { /*
       const username = this.props.match.params.username
       this.setState({ isLoading: true })
 
@@ -139,7 +158,8 @@ export default class Profile extends Component {
         .finally(() => {
           this.setState({ isLoading: false })
         })
-    }
+      */}
+    // }
   }
 
   isLocal() {
@@ -179,10 +199,11 @@ export default class Profile extends Component {
             {/* This might be where we put the table in */}
               <Table></Table>
             </div>
-            {/* This might be where we put the table in */}
             
             {this.isLocal() &&
+              
               <div className="new-status">
+                {/*
                 <div className="col-md-12">
                   <textarea className="input-status"
                     value={this.state.newStatus}
@@ -190,12 +211,13 @@ export default class Profile extends Component {
                     placeholder="What's on your mind?"
                   />
                 </div>
+                */}
                 <div className="col-md-12 text-right">
                   <button
                     className="btn btn-primary btn-lg"
-                    onClick={e => this.handleNewStatusSubmit(e)}
+                    onClick={e => this.handleUploadNewFile(e)}
                   >
-                    Submit
+                    Upload New File
                   </button>
                 </div>
               </div>
